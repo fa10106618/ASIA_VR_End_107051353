d5361e5706edecdd596b097eecfa0d12579d6a24
fa10106618 <71679881+fa10106618@users.noreply.github.com>
2021-01-09 17:48:32 +0800

    01/09完

---
+++ VR_End_107051353/Assets/S/GameManager.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("VR");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
